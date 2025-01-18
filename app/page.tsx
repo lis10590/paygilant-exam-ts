70931d3f15269d6ff283b5640e9e943e6f11a90e
lis10590 <lisset10590@gmail.com>
2025-01-19 00:56:21 +0200

    fixed fetch location on posts component

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,5 @@
 import Posts from "./components/Posts";
+
 interface Post {
   userId: number;
   id: number;
@@ -6,10 +7,9 @@ interface Post {
   body: string;
 }
 
-const response = await fetch("https://jsonplaceholder.typicode.com/posts");
-const posts: Post[] = await response.json();
-
-export default function Home() {
+export default async function Home() {
+  const response = await fetch("https://jsonplaceholder.typicode.com/posts");
+  const posts: Post[] = await response.json();
   return (
     <div className="flex flex-col items-center justify-center">
       <h1 className="m-6 font-bold text-5xl">Posts</h1>
